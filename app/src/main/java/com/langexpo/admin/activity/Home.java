1f075fe52b5b1851b7eb568e3d31f11011f829ca
Ravi Darji <ravi.darji2018@gmail.com>
2019-10-21 17:28:26 -0400

    - New Home activity added

---
+++ app/src/main/java/com/langexpo/admin/activity/Home.java
@@ -0,0 +1,16 @@
+package com.langexpo.admin.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.langexpo.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
